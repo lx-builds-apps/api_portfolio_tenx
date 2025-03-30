42b8c857b6fe122ca5db8706ba1289ce97990faf
lx-builds-apps <lx.builds.apps@gmail.com>
2025-03-31 04:22:15 +0800

    chore: partial implementation of create task

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 import express from 'express';
 import TasksRouter from './routes/tasks.routes'
+
 const app = express();
 
 const port = 8080;
+
+app.use((_req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    next();
+});
 app.listen(port, (err: Error) => {
     if (err) {
         console.error(err);
     }
+
 
     app.use(TasksRouter);
 
--- src/routes/tasks.routes.ts
+++ src/routes/tasks.routes.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import bodyParser from "body-parser";
 const router = express.Router();
 
-const tasks = [
+const jsonParser = bodyParser.json();
+
+const ERROR_MESSAGE_400_NO_BODY = "400 Bad Request: body cannot be empty.";
+const ERROR_MESSAGE_400_EMPTY_JSON_BODY = "400 Bad Request: body cannot be empty.";
+
+const tasks: Task[] = [
 
 ];
 
@@ -10,5 +16,52 @@ router.get(`/tasks`, (_req, res) => {
         tasks
     }));
 });
+
+interface Task {
+    id?: number;
+    name: string;
+    description: string;
+    status: "to-do" | "in-progress" | "done"
+}
+
+interface ErrorFeedback {
+    code: number;
+    message: string;
+}
+
+router.post(`/task`, jsonParser,  (req, res) => {
+    const validationFeedback = validateCreateTaskRequest(req.body);
+
+    if (validationFeedback) {
+        res.status(validationFeedback.code).send(validationFeedback.message);
+    }
+
+    const task: Task = req.body;
+
+    res.status(201).send(task);
+});
+
+/**
+ * validate body when attempting to create a Task
+ * @param {Task} body
+ * @return {false | ErrorFeedback}
+ */
+const validateCreateTaskRequest = (body: Task): false | ErrorFeedback =>  {
+    let feedback: ErrorFeedback | null = null;
+
+    if (!body) {
+        feedback = {
+            code: 400,
+            message: ERROR_MESSAGE_400_NO_BODY
+        }
+    } else if (!Object.entries(body).length) {
+        feedback = {
+            code: 400,
+            message: ERROR_MESSAGE_400_EMPTY_JSON_BODY
+        }
+    }
+
+    return feedback || false;
+}
 
 export default router;
